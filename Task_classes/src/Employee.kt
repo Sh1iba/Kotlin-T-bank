ef49a36c9e3f5950d300aeff2b62d6c58acf7076
Zafar Ibrogimov <zafar_ibrogimov@mail.ru>
2024-10-23 18:36:21 +0300

    complete first task

---
+++ Task_classes/src/Employee.kt
@@ -0,0 +1,52 @@
+open class Employee {
+
+    fun work() {
+        println("Усердно работаю")
+    }
+
+    open fun haveLunch() {
+        println("Иду на обед в 13.00")
+    }
+
+    open fun goHome() {
+        println("Иду домой в 19.00")
+    }
+}
+
+
+class Programmer() : Employee() {
+    override fun haveLunch() {
+        super.haveLunch()
+        println("А потом еще и за кофеечком")
+    }
+}
+
+
+class Designer : Employee() {
+
+    override fun haveLunch() {
+        println("Иду на обед в 15.00")
+    }
+}
+
+
+class Teamlead : Employee() {
+
+    override fun haveLunch() {
+        println("Сегодня пропущу обед")
+    }
+}
+
+// TODO здесь можно создать новый класс, который будет наследоваться от класса Employee. Не забудьте переопределить методы haveLunch() и goHome()
+class Fighter : Employee() {
+
+    override fun haveLunch() {
+        super.haveLunch()
+        println("Пойду на тренировку в 17:00")
+    }
+
+    override fun goHome() {
+        println("Приму душ")
+        super.goHome()
+    }
+}
